25efe83659df26f1421cc632024614775309f3aa
StevenSchnack <131918656+StevenSchnack@users.noreply.github.com>
2024-09-25 11:28:25 -0700

    Delete Security.php

--- Security.php
+++
@@ -1,8 +0,0 @@
-<?php
-  if (!isset($_SERVER['HTTPS'])) {
-    $url = 'https://' . $_SERVER['HTTP_HOST'] .
-           $_SERVER['REQUEST_URI'];  // starts with /...
-    header("Location: " . $url);  // Redirect - 302
-    exit;                         // should be before any output
-  }                               // 
-?>
